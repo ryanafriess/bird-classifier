f69e45f826db0d0f6cf9f4ef239441ea4929eed9
Ryan <friess@umich.edu>
2025-01-25 19:32:01 -0500

    second commit

--- bird.py
+++ bird.py
@@ -1,1 +1,14 @@
-print("hello world")
+import PIL
+import pathlib
+
+data_dir = pathlib.Path('nabirds/images/')  # Replace with the actual path
+
+print(data_dir)
+print(data_dir.exists())
+
+
+image_count = len(list(data_dir.glob('**/*.jpg')))
+print("Image count:", image_count)
+
+# roses = list(data_dir.glob('/*'))
+# PIL.Image.open(str(roses[0]))
