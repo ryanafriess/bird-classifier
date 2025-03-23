6a0ac08111829db67a5e13bb11d15a7c07df94d9
Ryan <friess@umich.edu>
2025-03-23 17:46:59 -0400

    added pytorch model

--- model.py
+++ model.py
@@ -3,8 +3,11 @@ import torchvision
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
-from torch.nn.functional import F
+import torch.nn.functional as F
+import torch.nn as nn
+import torch.optim as optim
 import matplotlib.pyplot as plt
+import time
 
 
 transforms = transforms.Compose([
@@ -12,10 +15,21 @@ transforms = transforms.Compose([
     transforms.ToTensor()
 ])
 
-dataset = ImageFolder(root="images/", transform=transforms)
+# dataset = ImageFolder(root="images/", transform=transforms)
+dataset = ImageFolder(root="bird_data/images/", transform=transforms) 
+
+dataset_length = len(dataset)
+train_size = int(0.8 * dataset_length)
+val_size = dataset_length - train_size
+train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
+
 
 # Create DataLoader for batch processing
 dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+# Create DataLoader for training and validation
+trainloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+valloader = DataLoader(val_dataset, batch_size=32, shuffle=True)
 
 images, labels = next(iter(dataloader))
 
@@ -30,34 +44,37 @@ class CNN(torch.nn.Module):
         super(CNN, self).__init__()
         
         self.conv1 = torch.nn.Conv2d(in_channels = 3, out_channels = 16, kernel_size = 3, padding = 1)
-        self.conv1 = torch.nn.Conv2d(in_channels = 16, out_channels = 48, kernel_size = 3, padding = 1)
+        self.conv2 = torch.nn.Conv2d(in_channels = 16, out_channels = 48, kernel_size = 3, padding = 1) #ask about naming
 
         self.bn1 = torch.nn.BatchNorm2d(16)
         self.bn2 = torch.nn.BatchNorm2d(48)
 
         self.pool = torch.nn.MaxPool2d(kernel_size = 2, stride = 2)
 
-        self.fc1 = torch.nn.Linear(48 * 28 * 28 * 28, 512)
+        # self.fc1 = torch.nn.Linear(48 * 28 * 28 * 28, 512)
+        self.fc1 = torch.nn.Linear(48 * 56 * 56, 512)
         
         self.fc2 = torch.nn.Linear(512, 200)
 
-    def foward(self, x):
+    def forward(self, x):
         # Block 1
-        x = self.conv1
-        x = self.bn1
+        x = self.conv1(x) #mention x fix
+        x = self.bn1(x)
         x = F.relu(x)
-        x = self.pool
+        x = self.pool(x)
         
         # Block 2
-        x = self.conv2
-        x = self.bn2
+        x = self.conv2(x)
+        x = self.bn2(x)
         x = F.relu(x)
-        x = self.pool
+        x = self.pool(x)
 
         # Fully Connected Layers
-        x = self.fc1
+        # print(x.shape)
+        x = x.view(x.size(0), -1) #flatten (ill figure out why later)
+        x = self.fc1(x)
         x = F.relu(x)
-        x = self.fc2
+        x = self.fc2(x)
         
 
         return x
@@ -66,13 +83,16 @@ class CNN(torch.nn.Module):
 
 model = CNN()
 epochs = 5
-optimizer = ??
-loss_func = ??
+optimizer = optim.Adam(model.parameters(), lr=0.001) #Adam
+loss_func = nn.CrossEntropyLoss() #CrossEntropyLoss for multi-class classification (but only one class per image)
 
+print("Training started...")
+start_time = time.time()
+placeholder_time = time.time()
 for n in range(epochs):
     run_loss = 0.0
 
-    for images, labels in dataloader: #replace dataloader with dataloader(training)
+    for images, labels in trainloader: 
         #forward prop
         outputs = model(images)
         loss = loss_func(outputs, labels) #define loss_func ()
@@ -83,13 +103,20 @@ for n in range(epochs):
         optimizer.step()
         run_loss += loss.item()
 
-    print("Epoch #" + str(n+1) + ", Loss: " + str(run_loss /len(dataloader))) #replace dataloader with dataloader(training)
+    print("Epoch #" + str(n+1) + ", Loss: " + str(run_loss /len(trainloader))) #average loss
+    epoch_time = time.time() - placeholder_time
+    print("Epoch completed after ", epoch_time, "seconds")
+    placeholder_time = time.time()
+    
 print("Training Over. Validation beginning...")
+total_train_time = time.time() - start_time
+print("Training over after ", total_train_time, "seconds")
+torch.save(model.state_dict(), 'bird_model_weights.pth')
 
 correct_count = 0
 total_count = 0
 
-for images, labels in dataloader: #replace with dataloader(validate)
+for images, labels in valloader: 
     ouptuts = model(images)
     _ , max_output = torch.max(outputs, 1)
     total_count += 32
