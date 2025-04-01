32a73d33aa2e8ff3e2c69f4dc00d0708e3e22868
Ryan <friess@umich.edu>
2025-04-01 18:26:35 -0400

    update model.py

--- model.py
+++ model.py
@@ -2,12 +2,13 @@ import torch
 import torchvision
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 import torch.nn.functional as F
 import torch.nn as nn
 import torch.optim as optim
 import matplotlib.pyplot as plt
 import time
+from collections import Counter
 
 
 transforms = transforms.Compose([
@@ -17,23 +18,39 @@ transforms = transforms.Compose([
 
 # dataset = ImageFolder(root="images/", transform=transforms)
 dataset = ImageFolder(root="bird_data/images/", transform=transforms) 
+NUM_CLASSES = len(dataset.classes)
+# print(len(dataset.classes))
+
+
+
 
 dataset_length = len(dataset)
-train_size = int(0.8 * dataset_length)
-val_size = dataset_length - train_size
+print("Dataset length:", dataset_length)
+train_size = int(0.6 * dataset_length)
+val_size = int(0.2 * dataset_length)
+test_size = dataset_length - train_size - val_size
 
-torch.manual_seed(42)
-train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+torch.manual_seed(50)
+train_dataset, test_dataset, val_dataset = random_split(dataset, [train_size, test_size, val_size])
 
+# print("Validation set size:", len(val_dataset))
+# print("Sample train labels:", [train_dataset[i][1] for i in range(5)])
+# print("Sample val labels:", [val_dataset[i][1] for i in range(5)])
+
+#SubsetRandomSampler
 
 # Create DataLoader for batch processing
 dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
 # Create DataLoader for training and validation
 trainloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+testloader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 valloader = DataLoader(val_dataset, batch_size=32, shuffle=False)
 
-images, labels = next(iter(dataloader))
+val_labels = [val_dataset[i][1] for i in range(len(val_dataset))]
+print(Counter(val_labels))
+
+# images, labels = next(iter(dataloader))
 
 # print(batch)
 # for images, labels in dataloader:
@@ -56,7 +73,7 @@ class CNN(torch.nn.Module):
         # self.fc1 = torch.nn.Linear(48 * 28 * 28 * 28, 512)
         self.fc1 = torch.nn.Linear(48 * 56 * 56, 512)
         
-        self.fc2 = torch.nn.Linear(512, 200)
+        self.fc2 = torch.nn.Linear(512, NUM_CLASSES)
 
     def forward(self, x):
         # Block 1
@@ -91,6 +108,7 @@ def train_model(model = None, epochs = 5, save = True): #returns a model and sav
     start_time = time.time()
     placeholder_time = time.time()
     for n in range(epochs):
+        model.train()
         run_loss = 0.0
 
         for images, labels in trainloader: 
@@ -104,12 +122,31 @@ def train_model(model = None, epochs = 5, save = True): #returns a model and sav
             optimizer.step()
             run_loss += loss.item()
 
-        print("Epoch #" + str(n+1) + ", Loss: " + str(run_loss /len(trainloader))) #average loss
+        print("Epoch #" + str(n+1) + ", Loss: " + str(run_loss / len(trainloader))) #average loss
         epoch_time = time.time() - placeholder_time
         print("Epoch completed after ", epoch_time, "seconds")
         placeholder_time = time.time()
         
-    print("Training Over. Validation beginning...")
+        ### epoch validation ###
+        model.eval()
+        pred_counts = []
+        correct_count = 0
+        total_count = 0
+        with torch.no_grad():
+            for images, labels in valloader:
+                outputs = model(images)
+                _, max_output = torch.max(outputs, 1)
+                pred_counts.extend(max_output.tolist())
+                total_count += labels.size(0)
+                # correct_count += sum(1 for i in range(len(max_output)) if max_output[i] == labels[i])
+                correct_count += (max_output == labels).sum().item()
+
+        val_accuracy = 100 * correct_count / total_count
+        print("Validation Accuracy (after epoch " + str(n+1) +"): " + str(val_accuracy))
+        print("Predicted class distribution:", Counter(pred_counts))
+        
+        
+    # print("Training Over. Validation beginning...")
     total_train_time = time.time() - start_time
     print("Training over after ", total_train_time, "seconds")
     if(save): torch.save(model.state_dict(), 'bird_model_weights.pth')
@@ -121,24 +158,26 @@ def load_model(): #loads model from 'bird_model_weights.pth' and returns
     model.load_state_dict(torch.load('bird_model_weights.pth'))
     return model
 
-def validate_model(model, valloader): #runs validation on model given valloader
+def test_model(model, testloader): #runs validation on model given valloader
     model.eval()
     correct_count = 0
     total_count = 0
 
     with torch.no_grad():
-        for images, labels in valloader: 
+        for images, labels in testloader: 
             outputs = model(images)
             _ , max_output = torch.max(outputs, 1)
             total_count += labels.size(0)
-            correct_count += sum(1 for i in range(len(max_output)) if max_output[i] == labels[i])
+            # correct_count += sum(1 for i in range(len(max_output)) if max_output[i] == labels[i])
+            correct_count += (max_output == labels).sum().item()
+
 
     accuracy = 100 * correct_count / total_count
     print("Test Accuracy: " + str(accuracy))
 
 def main():
-    model = load_model()
-    model = train_model(model=model, epochs=5, save=True)
-    validate_model(model, valloader)
+    # model = load_model()
+    model = train_model(model=None, epochs=2, save=True)
+    test_model(model, testloader)
     
 if __name__ == '__main__': main()
